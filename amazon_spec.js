86353f03d03857aa68df05c8c0112c2d261af94a
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-09-06 13:48:04 +0100

    Add files via upload

--- amazon_spec.js
+++ amazon_spec.js
@@ -1,25 +1,29 @@
 /// <reference types="cypress" />
 
-describe("Amazon Project", ()=>{
+describe("Amazon Project", () => {
+    beforeEach(() => {
+        cy.visit("https://www.amazon.in");
+    });
 
-    beforeEach(()=>{
-        cy.visit("https://amazon.in")
-    })
+    it("Search Product", () => {
+        cy.get("#searchDropdownBox").select("Electronics", { force: true });
 
-    it("Search Product", ()=>{
+        cy.get("#twotabsearchtextbox").type("Apple Watch");
 
-        cy.get("#searchDropdownBox").select("Electronics", {force:true})
+        cy.get("#nav-search-submit-button").click();
 
-        cy.get("#twotabsearchtextbox").type("Apple Watch")
+        cy.get('[data-component-type="s-search-result"]').as("products");
 
-        cy.get("#nav-search-submit-button").click()
+        cy.get("@products")
+            .eq(5)
+            .invoke("text")
+            .then((productText) => {
+                cy.log(productText);
+            });
 
-        cy.get('[data-component-type="s-search-results"]').as("products")
-
-        cy.get("@products").eq(5).invoke('text').then(productText => {
-
-            cy.log(productText)
-
-})
-})
-})
+        cy.get("@products").each(($el, index, $list) => {
+            cy.wrap($el).scrollIntoView();
+            cy.log("Index : " + index + " and the product is " + $el.text());
+        });
+    });
+});
